2216c114246a89d325becb56c3a27a0a7b2e0179
GryffGryff <39925712+GryffGryff@users.noreply.github.com>
2022-02-12 12:00:17 -0800

    Initial Commit

---
+++ AskForHelp-IOS/AskForHelp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskForHelp_IOSApp.swift
+//  AskForHelp-IOS
+//
+//  Created by Arun Mankoff-Dey on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AskForHelp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
